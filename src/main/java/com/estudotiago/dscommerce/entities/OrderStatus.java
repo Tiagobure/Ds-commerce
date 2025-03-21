3250633084d25e81e2882a5c88f798a8c8f37b26
Tiago santos palma <tiagosantos.mr@gmail.com>
2025-03-21 13:03:43 -0300

    Order

---
+++ src/main/java/com/estudotiago/dscommerce/entities/OrderStatus.java
@@ -0,0 +1,12 @@
+package com.estudotiago.dscommerce.entities;
+
+public enum OrderStatus {
+	
+        WAITING_PAYMENT,
+        PAID,
+        SHIPPED,
+        DELIVERED, 
+        CANCELED;
+        
+
+}
--- src/main/java/com/estudotiago/dscommerce/entities/User.java
+++ src/main/java/com/estudotiago/dscommerce/entities/User.java
@@ -1,11 +1,14 @@
 package com.estudotiago.dscommerce.entities;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,10 @@ public class User {
 	private LocalDate birthDate;
 	private String password;
 	
+	// um para muitos, colecao
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -94,6 +101,11 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 	
 	
