6d1892f58f167c92bbc2eefb25ac684b26a53f6a
Tiago santos palma <tiagosantos.mr@gmail.com>
2025-03-27 17:51:58 -0300

    POST insert

--- src/main/java/com/estudotiago/dscommerce/controllers/ProductController.java
+++ src/main/java/com/estudotiago/dscommerce/controllers/ProductController.java
@@ -2,11 +2,12 @@ package com.estudotiago.dscommerce.controllers;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +31,13 @@ public class ProductController {
 	@GetMapping
 	public Page<ProductDTO> findByAll(Pageable pageable) {
 		Page<ProductDTO> dto = service.findAll(pageable);
+		return dto;
+
+	}
+	
+	@PostMapping
+	public ProductDTO insert(@RequestBody ProductDTO dto) {
+		dto = service.insert(dto);
 		return dto;
 
 	}
